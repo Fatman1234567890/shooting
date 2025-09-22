150883fa58361fc87a2e807f84129fc9025cc3dc
Fatman1234567890 <bnt-970-dcr@e-net.nara.jp>
2025-09-22 06:14:24 +0000

    球を撃つ

---
+++ game/main.js
@@ -0,0 +1,83 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+
+const player = {
+    x: canvas.width / 2 - 15,
+    y: canvas.height - 60,
+    width: 30,
+    height: 30,
+    color: "yellow"
+};
+
+
+const bullets=[];
+const BULLETS_SPEED=-10;
+
+function tryShoot(now){
+    bullets.push({
+        x:player.x,
+        y:player.y,
+        width:5,
+        height:5,
+        vy:BULLETS_SPEED,
+    })
+}
+
+
+
+// fillRect(x座標(横), y座標(縦), 横幅, 縦幅)
+
+//自分のキャラクターに見立てた四角形をとりあえず作ってみてください。
+
+// width="480" height="640"
+
+let tama = 0;
+window.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft") {
+      if( player. x >10){
+    player.x-=10; 
+      } 
+    } else if (e.key === "ArrowRight") {
+        if(player.x < canvas.width - player.width - 10){
+      player.x += 10;
+        }
+    } else if (e.code === "Space") {
+     tryShoot();
+    }
+});
+function update(){
+ for(let i=bullets.length-1;i>=0;i--){
+        const bullet=bullets[i];
+         bullet.y+=bullet.vy;
+         if(bullet.y<0){
+    bullets.splice(i,1);
+    }
+   }
+}
+
+function draw(){
+ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+     ctx.fillStyle = player.color;
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+
+     ctx.fillStyle = "white";
+     for(let i=0;i<bullets.length;i++){
+        const bullet=bullets[i];
+      ctx.fillRect(bullet.x,bullet.y,bullet.width,bullet.height);
+}
+}
+
+
+
+
+
+function gameLoop() {
+    update();
+    draw();
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
